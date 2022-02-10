4832de48819ebe3821f46e3c10ddf8635d92c1e8
ASWLaunchs <1732747279@qq.com>
2022-02-10 15:47:20 +0800

    finished the merge sorted array.

--- easy/Merge Sorted Array.go
+++ easy/Merge Sorted Array.go
@@ -1,13 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
-func merge(nums1 []int, m int, nums2 []int, _ int) {
-	copy(nums1[m:], nums2)
-	sort.Ints(nums1)
+//method1
+// func merge(nums1 []int, m int, nums2 []int, _ int) {
+// 	copy(nums1[m:], nums2)
+// 	sort.Ints(nums1)
+// 	fmt.Println(nums1)
+// }
+
+//method2
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	sorted := make([]int, 0, m+n)
+	p1, p2 := 0, 0
+	for {
+		if p1 == m {
+			sorted = append(sorted, nums2[p2:]...)
+			break
+		}
+		if p2 == n {
+			sorted = append(sorted, nums1[p1:]...)
+			break
+		}
+		if nums1[p1] < nums2[p2] {
+			sorted = append(sorted, nums1[p1])
+			p1++
+		} else {
+			sorted = append(sorted, nums2[p2])
+			p2++
+		}
+	}
+	copy(nums1, sorted)
 	fmt.Println(nums1)
 }
 
